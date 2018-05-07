d77ba8128ad9486f69250dc19f33657533940fa3
LeoVal <leo.valente.114@gmail.com>
2018-05-07 23:48:37 +0100

    first

---
+++ preprocess.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(provide add-active-token def-active-token process-string)
+
+(define (add-active-token)
+  values "")
+(define (def-active-token)
+  values "")
+(define (process-string string)
+  values string)
---
+++ tests.rkt
@@ -0,0 +1,49 @@
+#lang racket
+(require "preprocess.rkt")
+
+(define verbose #f)
+
+; Run all tests in the given folder.
+; recursive calls ensure nested folders are scanned
+(define (run-tests testdir)
+  (for ([path (directory-list testdir)])
+    (run-file-or-go-deeper (build-path testdir path))))
+
+; If its a file, try and run it, else iterate over its contents.
+(define (run-file-or-go-deeper path)
+  (if (directory-exists? path)
+      (run-tests path)
+      (execute-test path)))
+
+; Print the test result
+(define (execute-test path)
+  (let ([string-path (path->string path)])
+  (when (string-suffix? string-path ".in")
+  (display (string-append "executing test: [" string-path "]" (compare-outputs path) "\n")))))
+
+; Does a comparison of the contents of the processed in-file
+; with the contents of the expected out-file.
+; If verbose flag is true then the expectations and result will be printed
+(define (compare-outputs path)
+  (letrec ([base-file (substring (path->string path) 0 (- (string-length (path->string path)) 3))]
+      [in-file (string->path (string-append base-file ".in"))]
+      [out-file (string->path (string-append base-file ".out"))]
+      [in-string  (file->string (path->string in-file))]
+      [out-string (file->string (path->string out-file))]
+      [process-result (process-string in-string)])
+
+    ; Print result and clean windows line-endings
+    (if (equal? process-result out-string)
+        (values " \u2713")
+        (values (string-append "\u2718"
+                               (if (equal? verbose #t)
+                                   (string-append "\n======== output was =========\n"
+                                                  (string-replace process-result "\r\n" "\n")
+                                                  "\n================================================\n"
+                                                  "============= expectation was ==================\n"
+                                                  (string-replace out-string "\r\n" "\n"))
+                                   (values "")))))))
+
+
+;; Run the tests here
+(run-tests (path->complete-path (build-path (current-directory) "resources")))
