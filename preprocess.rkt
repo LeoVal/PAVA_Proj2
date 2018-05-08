a840fe7e2e50bb50ec59d2e23bd021fa9e3c6bfc
LeoVal <leo.valente.114@gmail.com>
2018-05-08 01:11:14 +0100

    more regex

--- preprocess.rkt
+++ preprocess.rkt
@@ -12,7 +12,14 @@
 
 ;;;;; Some regex here
 
-;;: Alias matcher
+;;: Alias matchers
+
 ;; Capture the alias name and the matching expression
 ;; under the named groups 'aliasName' and 'aliasExpr'
 ; alias[^\S]+(?<aliasName>[^=\s]+)[\s+]*=[\s+]*(?<aliasExpr>[^;]*);
+; example: https://regex101.com/r/XYwvMg/2
+
+;; To detect the alias later we can use \b to match a word boundary.
+;; This example captures both strings between quotes and aliases named.
+;; With some logic we can just ignore the quotes and proceed to replace the bound word tokens.
+; example: https://regex101.com/r/5m6Pir/1
